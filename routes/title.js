6986d98966364f015d81ef3eebd160ebe6fbdd39
ReneP8 <rene-baving@web.de>
2022-08-04 19:32:59 +0200

    fixed issue not parsing title from body

--- routes/title.js
+++ routes/title.js
@@ -1,18 +1,17 @@
+import axios from 'axios';
 import { Router } from 'express';
-import request from 'request';
 const router = Router();
 
 router.get('/title', async (req, res) => {
     const { url } = req.query;
     try {
-        request(url, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send({ title: body });
-            } else {
+        axios.get(url)
+            .then((response) => {
+                res.send({ title: parseTitle(response.data) });
+            }).catch(error => {
                 console.dir(error);
                 res.status(500).send({ error: error });
-            }
-        })
+            })
     } catch (error) {
         console.dir(error);
         res.status(500).send({ error: error.message });
@@ -20,7 +19,8 @@ router.get('/title', async (req, res) => {
 });
 
 const parseTitle = (body) => {
-    let match = body.match(/<title>([^<]*)<\/title>/)
+    console.log(body);
+    let match = body.match(/<title.*>(.*?)<\/title>/)
     if (!match || typeof match[1] !== 'string')
         throw new Error('Unable to parse the title tag')
     return match[1]
